76cbd226a5864e0611e56c0bdab474310a99ca19
AsHiK-M(C.D) <ashik018361@gmaill.com>
2023-11-04 16:19:43 +0600

    adda nesw data and set local server

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const express = require('express')
 const app = express();
-const cors=require('cors')
+const cors = require('cors')
 const port = 5000;
 
-const categories =require('./data/categories.json')
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 
 app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello The News Dragon Server Site')
 })
+
 app.get('/categories', (req, res) => {
     res.send(categories);
+})
+
+
+
+/// news all aitem show
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+// news spacipick Id data lode 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedId = news.find(n => n._id === id)
+    res.send(selectedId)
+})
+
+// categories wais data lode (categories theke id silect korbo then oi id onosare categories_id data show korbo)
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
+
 })
 
 
@@ -21,6 +51,7 @@ app.get('/categories', (req, res) => {
 
 
 
-app.listen(port, ()=>{
-console.log("the News Site Runn", {port})
+
+app.listen(port, () => {
+    console.log("the News Site Runn", { port })
 })
